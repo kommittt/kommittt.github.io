62c3f0637bdbf803f44fa97c73f74f3e9dae1d72
kommit <iwanttocommittoasterbath09@gmail.com>
2025-09-20 16:16:25 +0200

    shitass mobile

--- pona/fuckshitpiss.js
+++ pona/fuckshitpiss.js
@@ -126,13 +126,16 @@ function viewmodeToggle() {
 // search button thing input yeah keypress whatever the FUCK
 searchButton.addEventListener('click', startDigginInYoButtTwin);
 searchInput.addEventListener('keydown', function(event) {
-    if (event.code === 'Enter') {
+    if (event.key === 'Enter') {
+        startDigginInYoButtTwin();
+    }
+    if (event.key === " ") {
         startDigginInYoButtTwin();
     }
     if (event.code === "Space") {
         startDigginInYoButtTwin();
     }
-    if (event.code === "Backspace") {
+    if (event.key === "Backspace") {
         startDigginInYoButtTwin();
     } // idk if this backspace thing is even working :(
 
